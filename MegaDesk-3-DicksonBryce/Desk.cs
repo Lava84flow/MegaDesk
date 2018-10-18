d590c0076d6e02aa744c999ce855274370882d34
Lava84flow <Lava84flow@users.noreply.github.com>
2018-10-18 16:34:46 -0400

    Update MegaDesk-3-DicksonBryce/Desk.cs
    added struct section for reguirements

--- MegaDesk-3-DicksonBryce/Desk.cs
+++ MegaDesk-3-DicksonBryce/Desk.cs
@@ -36,4 +36,39 @@ namespace MegaDesk_3_DicksonBryce
         }
 
     }
+
+    // I think this is right, let me know if you guys think this is not how structs work...
+    // The compiler didn't even sort of whine, so I have no idea.
+    /*
+    public struct DeskStruct
+    {
+        #region Member Vars
+
+        //Desk Description
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int Drawers { get; set; }
+        public Material DeskMaterial { get; set; }
+        public int Area { get; set; }
+
+        #endregion
+
+        //Desk Constraints
+        public const int MINWIDTH = 24;
+        public const int MAXWIDTH = 96;
+        public const int MINDEPTH = 12;
+        public const int MAXDEPTH = 48;
+
+        //Matrials
+        //[Flags]
+        public enum Material
+        {
+            Oak = 200,
+            Laminate = 100,
+            Pine = 50,
+            Rosewood = 300,
+            Veneer = 125
+        }
+    }
+    */
 }
